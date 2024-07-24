418344e2eb1ca08522c7d659d49b2e07aecee759
Noveleader <jhaankush992@gmail.com>
2024-07-24 22:05:16 +0530

    updated Navbar and Main section of the website

---
+++ src/app/components/Name.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "../styles/Name.css";
+
+const Name: React.FC = () => {
+  return (
+    <div className="name">
+      <p className="mainName">ANKUSH JHA</p>
+      <p className="description">A FULL STACK BLOCKCHAIN DEVELOPER</p>
+    </div>
+  );
+};
+
+export default Name;
---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/Navbar.css"
+
+const Navbar: React.FC = () => {
+    return (
+        <div className="navbar"> 
+            <nav>
+                <p>@noveleader</p>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
