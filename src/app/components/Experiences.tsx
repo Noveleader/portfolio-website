e85481e780d13f3a1c242a9519e94e121b3505de
Noveleader <jhaankush992@gmail.com>
2024-08-22 02:26:02 +0530

    updated experience and project section

--- src/app/components/Experiences.tsx
+++ src/app/components/Experiences.tsx
@@ -1,34 +1,9 @@
 "use client";
 import React from "react";
 import "../styles/Experiences.css";
+import { experience } from "../utils/data";
 
 const Experiences: React.FC = () => {
-  const experience = [
-    {
-      name: "FXDX (Backend Developer)",
-      logo: "coinsLogo/fxdx.png",
-      description:
-        "Led the v2 subgraph creation and migration to Envio, enhanced data retrieval, developed real-time price APIs with Redis caching, designed backend keepers for asset management, and pitched our product to potential partners.",
-      duration: "May 2024 - July 2024",
-      url: "https://fxdx.exchange/",
-    },
-    {
-      name: "FXDX (Blockchain Developer)",
-      logo: "coinsLogo/fxdx.png",
-      description:
-        "Built the backend for the airdrop dashboard, optimized calculations with parallel queries, and created subgraphs for IDO contract testing and vesting tracking.",
-      duration: "August 2023 - December 2023",
-      url: "https://fxdx.exchange/",
-    },
-    {
-      name: "Kryptomerch (Community Manager)",
-      logo: "coinsLogo/kryptomerch.jpg",
-      description:
-        "I managed Kryptomerch's Discord server and contributed to the successful IDO of the DB Cooper NFT collection.",
-      duration: "August 2022 - May 2023",
-      url: "https://kryptomerch.io/",
-    },
-  ];
   return (
     <div className="Experiences">
       <p className="Heading">Experience</p>
--- src/app/utils/data.ts
+++ src/app/utils/data.ts
@@ -1,22 +1,49 @@
+export const experience = [
+  {
+    name: "FXDX (Backend Developer)",
+    logo: "coinsLogo/fxdx.png",
+    description:
+      "Led the v2 subgraph creation and migration to Envio, enhanced data retrieval, developed real-time price APIs with Redis caching, designed backend keepers for asset management, and pitched our product to potential partners.",
+    duration: "May 2024 - July 2024",
+    url: "https://fxdx.exchange/",
+  },
+  {
+    name: "FXDX (Blockchain Developer)",
+    logo: "coinsLogo/fxdx.png",
+    description:
+      "Built the backend for the airdrop dashboard, optimized calculations with parallel queries, created subgraphs for IDO contract testing and vesting tracking, and took various BD initiatives.",
+    duration: "August 2023 - December 2023",
+    url: "https://fxdx.exchange/",
+  },
+  {
+    name: "Kryptomerch (Community Lead)",
+    logo: "coinsLogo/kryptomerch.jpg",
+    description:
+      "I managed Kryptomerch's socials and contributed to the successful IDO of the DB Cooper NFT collection which was launched on Kryptomerch NFT marketplace.",
+    duration: "August 2022 - May 2023",
+    url: "https://kryptomerch.io/",
+  },
+];
+
 export const repositories = [
   {
     name: "GMX synthetics subgraph",
     description:
-      "This envio indexers indexes GMX smart contract clone the current behaviour of GMX synthetics subgraph",
+      "The envio indexer is the clone of GMX V2 current subgraph implementation. It uses envio which is a better alternative for indexing blockchain events due to faster events processing and syncing.",
     language: "TypeScript",
     url: "https://github.com/Noveleader/gmx-v2-subgraph-envio",
   },
   {
     name: "PeerLinkDex",
     description:
-      "The project is an Over-The-Counter DEX which lets it users to create an escrow arrangement to exchange tokens between each other",
+      "The project is an Over-The-Counter DEX which lets it users to create an escrow arrangement to exchange tokens between each other. This essentially allows users to create a trustless environment to exchange any ERC20s.",
     language: "Solidity, TypeScript",
     url: "https://github.com/Noveleader/PeerLinkDex",
   },
   {
     name: "DeriPulse",
     description:
-      "Deripulse is a crypto derivatives information hub built using next JS, offering real-time insights into on-chain protocol data using defillama API",
+      "Deripulse is a crypto derivatives information hub built using next JS, offering real-time insights into on-chain derivative protocols using Defilama APIs. The website let's it users discover new emerging protocols, high-yield liquidity pools, etc.",
     language: "JavaScript, CSS",
     url: "https://github.com/Noveleader/deripulse-app",
   },
@@ -32,13 +59,13 @@ export const repositories = [
   //   language: "Solidity, JavaScript",
   //   url: "https://github.com/Noveleader/ERC-20",
   // },
-  {
-    name: "Healthcare SSI",
-    description:
-      "App Implements SSI in healthcare by storing all the important details of patient on blockchain",
-    language: "Dart, JavaScript, Solidity",
-    url: "https://github.com/Noveleader/Hackathon-Flutter",
-  },
+  // {
+  //   name: "Healthcare SSI",
+  //   description:
+  //     "App Implements SSI (Self-sovereign identity) in healthcare by storing all the important details of patient on blockchain.",
+  //   language: "Dart, JavaScript, Solidity",
+  //   url: "https://github.com/Noveleader/Hackathon-Flutter",
+  // },
   // {
   //   name: "Hardhat NFT",
   //   description:
