2191b4dc30dd00da526f8b1503976cd5dfd64f13
Noveleader <jhaankush992@gmail.com>
2025-01-18 02:48:38 +0530

    update: added splash-cursor and magnetic-lines

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Image from "next/image";
 import Navbar from "./components/Navbar";
 import Name from "./components/Name";
@@ -6,10 +7,14 @@ import Prices from "./components/Prices";
 import Experiences from "./components/Experiences";
 import Projects from "./components/Projects";
 import Footer from "./components/Footer";
+import SplashCursor from "./components/SplashCursor";
+import MagnetLines from "./components/MagnetLines";
 
 export default function Home() {
   return (
     <div>
+      <SplashCursor />
+      {/* <MagnetLines /> */}
       <Navbar />
       <Name />
       {/* <CV /> */}
